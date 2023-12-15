729d66de14d89b4b9e55407c37f37c99c9826e59
hiimparkjiho <jiho2689@gmail.com>
2023-12-15 18:14:09 +0900

    front

--- project_front_test/src/main/java/com/ohgiraffers/viewtest/Controller/TestController.java
+++ project_front_test/src/main/java/com/ohgiraffers/viewtest/Controller/TestController.java
@@ -22,4 +22,39 @@ public class TestController {
     public String signUpView(){
         return "view/signUpView";
     }
+
+    @GetMapping("/terms_view")
+    public String termsView(){
+        return "view/termView";
+    }
+
+    @GetMapping("admin")
+    public String adminView(){
+        return "admin/adminView";
+    }
+
+    @GetMapping("admin/login")
+    public String adminLoginView(){
+        return "admin/adminLoginView";
+    }
+
+    @GetMapping("modify_profile")
+    public String modifyUserView(){
+        return "view/modifyProfileView";
+    }
+
+    @GetMapping("/modify_pass")
+    public String modifyPass(){
+        return "view/modifyPassView";
+    }
+
+    @GetMapping("inquiry")
+    public String Inquiry(){
+        return "view/inquiryView";
+    }
+
+    @GetMapping("/inquiry_succ")
+    public String inquirySucces(){
+        return "view/inquiryRegistSuccess";
+    }
 }
